c3a98f97c2c9e96563dc435125908c882ae71581
Brandi <blubrandi0@gmail.com>
2020-02-02 16:49:27 -0700

    CoreData
    Added CoreData with models.  Added CoreDataStack file.

---
+++ TheHoneymoonPlanner/TheHoneymoonPlanner/Data/Activity+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  Activity+CoreDataClass.swift
+//  TheHoneymoonPlanner
+//
+//  Created by Brandi Bailey on 2/2/20.
+//  Copyright © 2020 Jonalynn Masters. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Activity)
+public class Activity: Vacation {
+
+}
---
+++ TheHoneymoonPlanner/TheHoneymoonPlanner/Data/WishlistItem+CoreDataClass.swift
@@ -0,0 +1,16 @@
+//
+//  WishlistItem+CoreDataClass.swift
+//  TheHoneymoonPlanner
+//
+//  Created by Brandi Bailey on 2/2/20.
+//  Copyright © 2020 Jonalynn Masters. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(WishlistItem)
+public class WishlistItem: Wishlist {
+
+}
