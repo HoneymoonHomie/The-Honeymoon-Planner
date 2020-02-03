c29da22c915b85947e50c680197ad945a336e59a
Brandi <blubrandi0@gmail.com>
2020-02-03 02:47:51 -0700

    Core Data, User Defaults, and Budget
    Removed Budget Entity, set to user defaults.  Added slider to set budget.  Budget displaying on Settings scene.  Removed all other models and convenience files due to errors.  TODO:  Investigate core data errors and recreate files.

--- TheHoneymoonPlanner/TheHoneymoonPlanner/Data/CoreDataStack.swift
+++ TheHoneymoonPlanner/TheHoneymoonPlanner/Data/CoreDataStack.swift
@@ -11,30 +11,47 @@ import CoreData
 
 class CoreDataStack {
     //MARK: - Core Data Stack
+    
+    static let shared = CoreDataStack()
+    
     private init() {}
-    static var context: NSManagedObjectContext {
-        return persistentContainer.viewContext
-    }
-    static var persistentContainer: NSPersistentContainer = {
-        let container = NSPersistentContainer(name: "TheHoneymoonPlanner" )
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                print("Unresolved error \(error), \(error.userInfo)")
+    
+    lazy var container: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "TheHoneymoonPlanner")
+        container.loadPersistentStores(completionHandler: { _, error in
+            if let error = error {
+                fatalError("Failed to load persistent store: \(error)")
             }
         })
+        // May need to be disabled if dataset is too large for performance reasons
+        container.viewContext.automaticallyMergesChangesFromParent = true
         return container
     }()
-    // MARK: - Core Data Saving support
-    static func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+    
+    func saveContext(context: NSManagedObjectContext) throws {
+        //Placeholder in case something doesn't work
+        var closureError: Error?
+        context.performAndWait {
             do {
                 try context.save()
                 print("SAVED!!!")
             } catch {
-                let nserror = error as NSError
-                print("Unresolved error \(nserror), \(nserror.userInfo)")
+                NSLog("error saving moc: \(error)")
+                closureError = error
             }
+        }
+        if let error = closureError {
+            throw error
         }
     }
 }
+
+extension NSManagedObjectContext {
+    static let mainContext = CoreDataStack.shared.mainContext
+}
+
+
+
