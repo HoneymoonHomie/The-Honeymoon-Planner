ff7a6bc2098c3ae35540002d5665b26cdb93f15d
Jonalynn Masters <jonalynna.masters@gmail.com>
2020-02-03 17:11:42 -0800

    added Activity Start Date and Activity End Date action sheet date picker

--- TheHoneymoonPlanner/TheHoneymoonPlanner/View Controllers/ActivityViewController.swift
+++ TheHoneymoonPlanner/TheHoneymoonPlanner/View Controllers/ActivityViewController.swift
@@ -12,15 +12,36 @@ class ActivityViewController: UIViewController {
     
     @IBOutlet weak var activityNameTextField: UITextField!
     @IBOutlet weak var activityPriceTextField: UITextField!
-    @IBOutlet weak var dateTextField: UITextField!
     @IBOutlet weak var activityDescriptionTextView: UITextView!
     
+    @IBOutlet weak var startTextField: UITextField!
+    
+    @IBOutlet weak var endTextField: UITextField!
+    
+    private var startDatePicker: UIDatePicker?
+    private var endDatePicker: UIDatePicker?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        startDatePicker = UIDatePicker()
+            endDatePicker = UIDatePicker()
+            startDatePicker?.datePickerMode = .dateAndTime
+            endDatePicker?.datePickerMode = .dateAndTime
+            
+            startDatePicker?.addTarget(self, action: #selector(ActivityViewController.startDateChanged(datePicker:)), for: .valueChanged)
+             endDatePicker?.addTarget(self, action: #selector(ActivityViewController.endDateChanged(datePicker:)), for: .valueChanged)
+            
+            let startTapGesture = UITapGestureRecognizer(target: self, action: #selector(ActivityViewController.startViewTapped(gestureRecognizer:)))
+            
+            let endTapGesture = UITapGestureRecognizer(target: self, action: #selector(ActivityViewController.endViewTapped(gestureRecognizer:)))
+            
+            view.addGestureRecognizer(startTapGesture)
+            startTextField.inputView = startDatePicker
+            
+            view.addGestureRecognizer(endTapGesture)
+            endTextField.inputView = endDatePicker
+        }
         // Do any additional setup after loading the view.
-    }
     
     @IBAction func cancelButtonTapped(_ sender: UIButton) {
     }
@@ -28,6 +49,32 @@ class ActivityViewController: UIViewController {
     @IBAction func saveButtonTapped(_ sender: UIButton) {
     }
     
+    @objc func startViewTapped(gestureRecognizer: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    @objc func startDateChanged(datePicker: UIDatePicker) {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        
+        startTextField.text = dateFormatter.string(from: datePicker.date)
+        view.endEditing(true)
+    }
+    
+    @objc func endViewTapped(gestureRecognizer: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
+    @objc func endDateChanged(datePicker: UIDatePicker) {
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .medium
+        dateFormatter.timeStyle = .short
+        
+        endTextField.text = dateFormatter.string(from: datePicker.date)
+        view.endEditing(true)
+    }
+
 
     /*
     // MARK: - Navigation
