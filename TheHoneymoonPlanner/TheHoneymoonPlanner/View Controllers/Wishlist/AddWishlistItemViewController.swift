d4721ae6eec2fe29e0a4ccaf738004ef72d088d2
Brandi <blubrandi0@gmail.com>
2020-02-04 18:02:41 -0700

    Fully Rebuilt on newbranch
    Also added doneTapped and dismiss keyboard

--- TheHoneymoonPlanner/TheHoneymoonPlanner/View Controllers/Wishlist/AddWishlistItemViewController.swift
+++ TheHoneymoonPlanner/TheHoneymoonPlanner/View Controllers/Wishlist/AddWishlistItemViewController.swift
@@ -7,27 +7,92 @@
 //
 
 import UIKit
+import CoreData
 
-class AddWishlistItemViewController: UIViewController {
-
+class AddWishlistItemViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    var wishlist: Wishlist?
+    var wishlists: [Wishlist] = []
+    
+    @IBOutlet weak var wishlistNameTextField: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.wishlistNameTextField.delegate = self
+        
+        fetchWishlistItems()
+        
     }
     
-    @IBAction func cancelTapped(_ sender: Any) {
-        self.dismiss(animated: true, completion: nil)
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return wishlists.count
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+         let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell", for: indexPath)
 
+        let wishlist = wishlists[indexPath.row]
+        cell.textLabel?.text = wishlist.item
+
+               return cell
+    }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if (editingStyle == .delete) {
+            
+            let wishlist = wishlists[indexPath.row]
+            CoreDataStack.context.delete(wishlist)
+            
+            CoreDataStack.saveContext()
+            fetchWishlistItems()
+        }
+    }
+    
+    @IBAction func doneTapped(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    
+    func addWishlistItem() {
+
+        let wishlist = Wishlist(context: CoreDataStack.context)
+        wishlist.item = wishlistNameTextField.text
+        
+        CoreDataStack.saveContext()
+        fetchWishlistItems()
+    }
+    
+    func fetchWishlistItems() {
+        
+        let fetchRequest: NSFetchRequest<Wishlist> = Wishlist.fetchRequest()
+        do {
+            wishlists = try CoreDataStack.context.fetch(fetchRequest)
+            tableView.reloadData()
+            print(wishlists)
+            print(wishlists.count)
+        } catch {
+            print(error)
+        }
+    }
+    
+}
+
+extension AddWishlistItemViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        addWishlistItem()
+        wishlistNameTextField.text = ""
+        wishlistNameTextField.resignFirstResponder()
+        return true
+    }
 }
